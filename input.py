099b0a817665fdc720c69980161255e814fcd580
unknown <9giannis@gmail.com>
2023-06-01 17:49:09 +0300

    The program is done

--- input.py
+++ input.py
@@ -1,100 +1,79 @@
 from tkinter import *
 import tkinter as tk
 from datetime import date
+from tkinter import messagebox
 
 win= Tk()
+
 win.title('Workout Calendar')
 
 win.geometry("800x400+600+280")
 
-
-def workout_func():
-    workout.append(workout_entry1.get())
-    workout.append(workout_entry2.get())
-    workout.append(workout_entry3.get())
-    workout.append(workout_entry4.get())
-    workout.append(workout_entry5.get())
-    workout.append(workout_entry6.get())
-    workout.append(workout_entry7.get())
-    workout.append(workout_entry8.get())
-
-    print(workout)
-
 workout = []
 
+
+def workout_func():
+    
+    for i, entry in enumerate(workout_entries):
+        day = days[i]
+        workout_text = entry.get()
+        if workout_text == '':
+            messagebox.showwarning("Empty Workout", f"You need to fill in the workout for {day} before saving!")
+            return
+
+    res = messagebox.askquestion('Save Confirmation', 'Save the desired workout plan?')
+    if res == 'yes':
+        for i, entry in enumerate(workout_entries):
+            day = days[i]
+            workout_text = entry.get()
+            workout.append(f"{workout_text}")
+    elif res == 'no':
+        print("User cancelled without saving")
+        return
+    else:
+        print("Invalid response from user")
+        return
+
+    print(workout)
+    win.destroy()
+
+
+
+userd = date.today()
 #variables for the columms
 
 workout_label_main = tk.Label(win, text="Enter your workout routine",font=("Ariel",15,'bold'))
 workout_label_main.grid(row=0, column=1,pady=10)
 
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+workout_labels = []
+workout_entries = []
+for i , day in enumerate(days):
+    label = tk.Label(win, text=day + ":", font=("Arial", 13,'bold'))
+    label.grid(row=i + 1, column=0,sticky='e')
+    workout_labels.append(label)
+    
 
-workout_label1 = tk.Label(win, text="Monday:", font=("Arial", 12))
-workout_label1.grid(row=1, column=0)
-workout_entry1 = tk.Entry(win,width=50)
-workout_entry1.grid(row=1, column=1)
-
-workout_label2 = tk.Label(win, text="Tuesday:", font=("Arial", 12))
-workout_label2.grid(row=2, column=0, padx=10,pady=5)
-workout_entry2 = tk.Entry(win,width=50)
-workout_entry2.grid(row=2, column=1)
-
-workout_label3 = tk.Label(win, text="Wednesday:", font=("Arial", 12))
-workout_label3.grid(row=3, column=0, padx=10,pady=5)
-workout_entry3 = tk.Entry(win,width=50)
-workout_entry3.grid(row=3, column=1)
-
-workout_label4 = tk.Label(win, text="Thursday:", font=("Arial", 12))
-workout_label4.grid(row=4, column=0, padx=10,pady=5)
-workout_entry4 = tk.Entry(win,width=50)
-workout_entry4.grid(row=4, column=1)
-
-workout_label5 = tk.Label(win, text="Friday:", font=("Arial", 12))
-workout_label5.grid(row=5, column=0, padx=10,pady=5)
-workout_entry5 = tk.Entry(win,width=50)
-workout_entry5.grid(row=5, column=1)
-
-workout_label6 = tk.Label(win, text="Saturday:", font=("Arial", 12))
-workout_label6.grid(row=6, column=0, padx=10,pady=5)
-workout_entry6 = tk.Entry(win,width=50)
-workout_entry6.grid(row=6, column=1)
-
-workout_label7 = tk.Label(win, text="Sunday:", font=("Arial", 12))
-workout_label7.grid(row=7, column=0, padx=10,pady=5)
-workout_entry7 = tk.Entry(win,width=50)
-workout_entry7.grid(row=7, column=1)
-
-workout_label8 = tk.Label(win, text="Monday:", font=("Arial", 12))
-workout_label8.grid(row=8, column=0, padx=10,pady=5)
-workout_entry8 = tk.Entry(win,width=50)
-workout_entry8.grid(row=8, column=1)
+    entry = tk.Entry(win, width=50)
+    entry.grid(row=i + 1, column=1)
+    workout_entries.append(entry)
 
 
-Example = tk.Label(win, text="Examples:", font=("Arial", 12,'bold'))
-Example.grid(row=0, column=3,padx=10, sticky="w")
 
-Example = tk.Label(win, text="Monday: Cardio", font=("Arial", 12))
-Example.grid(row=1, column=3,padx=10,sticky="w")
+example_text = [
+    "Monday: Cardio",
+    "Tuesday: Weights/Push day",
+    "Wednesday: Pull exercises/Weights",
+    "Thursday: Rest",
+    "Friday: Boxing or bike riding",
+    "Saturday: Hiking or ab workout",
+    "Sunday: Leg workout"]
 
-Example = tk.Label(win, text="Tuesday: Weights/Push day ", font=("Arial", 12))
-Example.grid(row=2, column=3,padx=10,sticky="w")
+for i, text in enumerate(example_text):
+    example_label = tk.Label(win, text=text, font=("Arial", 12))
+    example_label.grid(row=i + 1, column=3, padx=10, sticky="w")
 
-Example = tk.Label(win, text="Wednesday: Pull exercises/Weights", font=("Arial", 12))
-Example.grid(row=3, column=3,padx=10,sticky="w")
 
-Example = tk.Label(win, text="Thursday: Rest", font=("Arial", 12))
-Example.grid(row=4, column=3,padx=10,sticky="w")
-
-Example = tk.Label(win, text="Friday: Boxing or bike riding", font=("Arial", 12))
-Example.grid(row=5, column=3,padx=10,sticky="w")
-
-Example = tk.Label(win, text="Saturday: Hiking or ab workout", font=("Arial", 12))
-Example.grid(row=6, column=3,padx=10,sticky="w")
-
-Example = tk.Label(win, text="Sunday: Leg workout", font=("Arial", 12))
-Example.grid(row=7, column=3,padx=10,sticky="w")
-
-Example = tk.Label(win, text="Rest: Its important to rest twice within 8 days! ", font=("Arial", 12),fg='red')
-Example.grid(row=8, column=3,padx=10,sticky="w")
 
 submit_button = Button(win, text="Submit", command=workout_func,width=10,font=("Arial", 12,'bold'),bg='black',fg='white')
 submit_button.grid(row=9, column=1, columnspan=2, pady=10)
--- main.py
+++ main.py
@@ -1,36 +1,35 @@
 from tkinter import *
 import tkinter as tk
 from datetime import date
-from input import workout_func, workout
+from input import workout_func,userd,workout
+import datetime
 
 
-workout = workout
 
 
-today = date(2023, 5, 31)
+today =  userd 
 
 win= Tk()
-win.title('')
+win.title('Workout of the day')
 #win.attributes('-alpha', 1)  # Set window transparency to 80% (slightly transparent)
 #win.wm_attributes("-transparentcolor","black")
 #win.overrideredirect(1)
-win.geometry("500x150+3300+100")
+
+
+win.geometry("+600+280")
 
 def workout_plan():
-    days_since_start = (date.today() - today).days
-    workout_index = days_since_start % len(workout)
-    #work = "Today's Workout:" +"\n"+ workout[workout_index]
+    current_day = datetime.datetime.today().weekday()
+    # Calculate the workout index based on the current day
+    workout_index = (current_day) % 7  # Offset the current day to align with the workout list
     work = workout[workout_index]
     label.config(text=work)
-    label.place(relx = 0.14,
-                rely = 0.8,
-                anchor= 'sw')
-    
+
 
 label = tk.Label(win,font=('Ariel 25 bold'),fg="white", bg="black")
-label.pack()
-label2 = tk.Label(win,text="Today's Workout:",font=('Ariel 25 bold'),fg="white", bg="black")
-label2.pack()
+label.grid(row=1,column=2,sticky='nsew')
+label2 = tk.Label(win,text="Today's Workout",font=('Ariel 25 bold'),fg="white", bg="black")
+label2.grid(row=0,column=2,sticky='nsew')
 workout_plan()
 win.wm_attributes('-toolwindow', 'True')
 win.configure(bg='white')
